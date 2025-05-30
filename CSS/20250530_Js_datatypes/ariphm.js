3eed8fc4464e65ad62f6a9e332b69b0e044f88ac
dasarian <coolairianz@gmail.com>
2025-05-30 21:14:10 +0300

    Friday class

---
+++ CSS/20250530_Js_datatypes/ariphm.js
@@ -0,0 +1,29 @@
+/**
+ * отработка арифметических орераторов
+ * создайте переменную и задайте ей любое числовое значение
+ * ИСПОЛЬЗУЕМ АРИФМЕТИКУ в 2 раза
+ * Вычтите 10
+ * Возьмите остаток от деления на 8
+ * Возведите в третью степень
+ * Увеличьте на 1 инкрементом
+ * Выведите результат
+ */
+
+let operator = 1000;
+operator = 1000;
+console.log(operator);
+
+operator = operator * 2;
+console.log(operator);
+
+operator = operator - 10;
+console.log(operator);
+
+operator = operator / 8;
+console.log(operator);
+
+operator = operator ** 3;
+console.log(operator);
+
+operator = operator ++;
+console.log(operator);
