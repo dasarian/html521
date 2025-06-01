92cd36793806845a141f9e47c1b52ef50eebc8f0
dasarian <coolairianz@gmail.com>
2025-06-01 13:00:34 +0300

    changed

--- Home Exercise/Home Work/20250529_Dz/time.js
+++ Home Exercise/Home Work/20250529_Dz/time.js
@@ -1,3 +1,4 @@
+
 let seconds = 45; 
 let totalSeconds = seconds + 580;
 
@@ -7,7 +8,6 @@ let remainingSeconds = totalSeconds % 60;    // Get the remaining seconds
 
 console.log(`Total time is ${minutes} minutes and ${remainingSeconds} seconds.`);
 
-
 Правда ли, что 5800 секунд больше часа? Больше 2х часов?
 
 const seconds = 5800;
@@ -16,7 +16,6 @@ const isMoreThanTwoHours = seconds > 7200; // 2 hours = 7200 seconds
 
 console.log(`Is 5800 seconds more than an hour? ${isMoreThanOneHour}`);
 console.log(`Is 5800 seconds more than two hours? ${isMoreThanTwoHours}`);
-
 
 Правда ли, что если цена составляет 230 рублей за штуку
     и купили 27 штук, то хватит 500 рублей? 5000 рублей? 7000 рублей?
@@ -33,9 +32,9 @@ console.log(`Is 500 rubles enough? ${is500Enough}`);
 console.log(`Is 5000 rubles enough? ${is5000Enough}`);
 console.log(`Is 7000 rubles enough? ${is7000Enough}`);
 
-
 Правда ли, что 5000 рублей хватит, а 7000 НЕ хватит 
 
 const is5000EnoughButNot7000 = is5000Enough && !is7000Enough;
 
 console.log(`Is it true that 5000 rubles will be enough, but 7000 will NOT be enough? ${is5000EnoughButNot7000}`);
+
