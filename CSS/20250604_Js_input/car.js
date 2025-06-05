5901f4916863ebb63bf730a23314620290f3f493
dasarian <coolairianz@gmail.com>
2025-06-05 10:06:40 +0300

    updated

---
+++ CSS/20250604_Js_input/car.js
@@ -0,0 +1,8 @@
+function start() {
+   
+    
+    let h =time.valueAsNumber* speed.valueAsNumber;
+    console.log(car.value ,h,'км/часа')
+    console.log(time.value,'час')
+    console.log(speed.value,'км/с')
+}
