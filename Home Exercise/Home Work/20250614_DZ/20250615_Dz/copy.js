21453a803b12019cf72d79fa6e78cb8bc45d3beb
dasarian <coolairianz@gmail.com>
2025-06-15 17:56:59 +0300

    updated

---
+++ Home Exercise/Home Work/20250614_DZ/20250615_Dz/copy.js
@@ -0,0 +1,4 @@
+function add_w() {
+const val = document.getElementById('input1').value;
+document.getElementById('input2').value = val;
+}
---
+++ Home Exercise/Home Work/20250614_DZ/20250615_Dz/text.js
@@ -0,0 +1,7 @@
+function add_can() {
+    
+let li = document.createElement('h2')
+
+header.appendChild(li)
+li.textContent='Ваш загаловка:   '+head.value
+}
