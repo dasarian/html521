f791608a5fc2cb5deb349cddb26c2ddee667faca
dasarian <coolairianz@gmail.com>
2025-06-25 22:09:53 +0300

    updated

---
+++ CSS/20250528_Js_start/20250625_arrays/arr_test.js
@@ -0,0 +1,20 @@
+/*1
+*/
+const imya = 'Вася'
+// Буквы нумеруются с нуля
+// Задание: вывести все буквы имени
+let bukva_nomer = 0
+while (bukva_nomer < 4) {
+    console.log(imya[bukva_nomer])
+    bukva_nomer++
+}
+
+/*2
+*/
+const name = 'Вася'
+let const_number = 1
+while (const_number < 4) {
+    console.log(name[const_number])
+    const_number+= 2
+}
+
