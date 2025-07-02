f9d2807d258b3c1d3570cea023c2cb53cc4ff4d4
dasarian <coolairianz@gmail.com>
2025-07-02 19:38:39 +0300

    updated

---
+++ Home Exercise/Home Work/20250627_Hw/8.js
@@ -0,0 +1,5 @@
+const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eva', 'Frank', 'Grace'];
+
+console.log('Первое имя:', names[0]);
+console.log('Последнее имя:', names[names.length - 1]);
+console.log('Количество имен:', names.length);
---
+++ Home Exercise/Home Work/20250627_Hw/9.js
@@ -0,0 +1,5 @@
+const numbers = [];
+for (let i = 1; i <= 1000; i++) {
+  numbers.push(i);
+}
+console.log(numbers);
