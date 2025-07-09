30fc800c7e52b85de63fa753ca3406765e238231
dasarian <coolairianz@gmail.com>
2025-07-09 19:52:27 +0300

    updated

---
+++ JavaScript/20250709_Functions/script.js
@@ -0,0 +1,11 @@
+// print() // вызов печати на принтер
+function something () {
+this.a = 9
+}
+let s = new something ()
+console.log (s.a)
+
+
+/** чтобы создать функцию :
+ * 1. ключевое слово function
+ * 2. 
