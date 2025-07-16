2aac894e874273b04d4c3e1f365d8a12463320fa
dasarian <coolairianz@gmail.com>
2025-07-16 21:14:50 +0300

    updated

--- JavaScript/Examination/work.js
+++ JavaScript/Examination/work.js
@@ -3,9 +3,6 @@ console.log('начинается');
 function showMessage() {
             alert('Кнопка нажата!');
         }
-            const tam = () => {
-    const inputField = document.getElementById('numberInput');
-    console.log(inputField.value);
-  };
+
 
      
