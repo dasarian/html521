a7e6ada8e9bfd5632ba4cf5d6717fea330c00346
dasarian <coolairianz@gmail.com>
2025-08-29 20:23:01 +0300

    updated

--- Python/13.08.25/20252008_Class/20250820_Hw/20250829_Bulls_Cows/bulls&cows_program.py
+++ Python/13.08.25/20252008_Class/20250820_Hw/20250829_Bulls_Cows/bulls&cows_program.py
@@ -1,12 +1,10 @@
 import random
 
 def generate_secret_number():
-    """Generate a random four-digit number with unique digits"""
     digits = random.sample(range(10), 4)
     return ''.join(map(str, digits))
 
 def count_bulls_and_cows(guess, secret):
-    """Counting bulls and cows"""
     bulls = 0
     cows = 0
     for i in range(4):
@@ -17,18 +15,11 @@ def count_bulls_and_cows(guess, secret):
     return bulls, cows
 
 def play_game():
-    """Basic logic of the game"""
     secret_number = generate_secret_number()
     attempts = 0
 
-    print("Welcome to the game 'Bulls and Cows'!")
-    print("I thought of a four-digit number with unique digits")
-    print("Your task is — guess it. After each attempt I will say, how many bulls and cows")
-
     while True:
         guess = input("Please enter four digit number: ").strip()
-
-        # Проверка корректности ввода
         if len(guess) != 4 or not guess.isdigit() or len(set(guess)) != 4:
             print("Error: enter a four digit number with unique digits.")
             continue
@@ -41,6 +32,5 @@ def play_game():
             break
         else:
             print(f"Bulls: {bulls}, Cows: {cows}")
-
 if __name__ == "__main__":
     play_game()
