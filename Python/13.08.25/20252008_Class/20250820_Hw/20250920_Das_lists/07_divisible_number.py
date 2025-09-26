b563783b3d946da47822c03fd325d3779ed27850
dasarian <coolairianz@gmail.com>
2025-09-26 12:12:01 +0300

    updated

--- Python/13.08.25/20252008_Class/20250820_Hw/20250920_Das_lists/07_divisible_number.py
+++ Python/13.08.25/20252008_Class/20250820_Hw/20250920_Das_lists/07_divisible_number.py
@@ -1,8 +1,8 @@
 lst = [7, 3, 13, 17, 14, 12, 21, 45, 77, 23, 16]
 divisible_numbers = []
 
-for x in lst:
-    if x % 7 != 0 and x % 3 != 0:
-        divisible_numbers.append(x)
+for num in lst:
+    if num % 7 != 0 and num % 3 != 0:
+        divisible_numbers.append(num)
 
 print(divisible_numbers)
