9494de1d1f1b330b679ad683d2fb5416beae8e26
dasarian <coolairianz@gmail.com>
2025-10-17 21:11:19 +0300

    updated

--- Python/20251014_HW_Das/20251017_oop_Class/Task_1.py
+++ Python/20251014_HW_Das/20251017_oop_Class/Task_1.py
@@ -33,26 +33,4 @@ class Person:
 
    
 
-    def working_days_given(self, holidays=None):
-        """
-        Возвращает количество рабочих дней, данных на выполнение задачи,
-        с учётом выходных (сб, вс) и праздников.
-        """
-        if holidays is None:
-            holidays = []
-
-        delta = self.deadline - self.dt
-        working_days = 0
-        for i in range(delta.days + 1):
-            current_day = self.dt + timedelta(days=i)
-            if current_day.weekday() < 5 and current_day not in holidays:
-                working_days += 1
-        return working_days
-
-    def __str__(self):
-        """Возвращает строковое представление задачи."""
-        status = "Выполнена" if self.done else "Не выполнена"
-        return (f"Описание: {self.description}\n"
-                f"Создана: {self.dt.strftime('%d.%m.%Y %H:%M')}\n"
-                f"Дедлайн: {self.deadline.strftime('%d.%m.%Y %H:%M')}\n"
-                f"Статус: {status}")
+    
