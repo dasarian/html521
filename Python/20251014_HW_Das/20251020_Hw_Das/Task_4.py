9668866826e242381ac9aeb96d793e977179a6b5
dasarian <coolairianz@gmail.com>
2025-10-20 20:18:20 +0300

    updated

--- Python/20251014_HW_Das/20251020_Hw_Das/Task_4.py
+++ Python/20251014_HW_Das/20251020_Hw_Das/Task_4.py
@@ -1,12 +1,39 @@
+# Дополнить класс "комната" функцией, считающей площадь её стен (без учета окон и дверей).
 # Дополнить класс "квартира" такой же функцией.
+class Room:
+    def __init__(self, length, width, height, windows_area=0, doors_area=0):
+        self.length = length
+        self.width = width
+        self.height = height
+        self.windows_area = windows_area
+        self.doors_area = doors_area
 
-class Квартира:
-    def __init__(self, комнаты):
-        self.комнаты = комнаты  
+    def walls_area(self):
+        total_walls = 2 * self.height * (self.length + self.width)
+        clean_walls = total_walls - (self.windows_area + self.doors_area)
+        return clean_walls
 
-    def площадь_стен(self):
-        общая_площадь = 0
-        for комната in self.комнаты:
-            общая_площадь += комната.площадь_стен()
-        return общая_площадь
-    
+class Apartment:
+    def __init__(self):
+        self.rooms = []
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    def walls_area(self):
+        total_area = 0
+        for room in self.rooms:
+            total_area += room.walls_area()
+        return total_area
+
+
+room1 = Room(length=5, width=4, height=3, windows_area=2, doors_area=1)
+room2 = Room(length=6, width=3, height=3, windows_area=1.5, doors_area=1)
+
+apartment = Apartment()
+apartment.add_room(room1)
+apartment.add_room(room2)
+
+print("Room 1 wall area:", room1.walls_area())
+print("Room 2 wall area:", room2.walls_area())
+print("Total apartment wall area:", apartment.walls_area())
--- Python/20251014_HW_Das/20251020_Hw_Das/Task_5.py
+++ Python/20251014_HW_Das/20251020_Hw_Das/Task_5.py
@@ -2,33 +2,57 @@
 # если задать ширину, длину и стоимость рулона (тоже класс) и 
 # количество рулонов должно быть строго целым.
 
-class Комната:
-    def __init__(self, длина, ширина, высота):
-        self.длина = длина
-        self.ширина = ширина
-        self.высота = высота
-    
-    def площадь_стен(self):
-        периметр = 2 * (self.длина + self.ширина)
-        площадь_стен_без_вычетов = периметр * self.высота
-        return итоговая_площадь
+class WallpaperRoll:
+    def __init__(self, width, length, price_per_roll):
+        self.width = width  
+        self.length = length  
+        self.price_per_roll = price_per_roll  
 
-class Обои:
-    def __init__(self, ширина_рулона, длина_рулона, цена_рулона):
-        self.ширина_рулона = ширина_рулона
-        self.длина_рулона = длина_рулона
-        self.цена_рулона = цена_рулона
+class Room:
+    def __init__(self, length, width, height, num_windows=0, window_area=0, num_doors=0, door_area=0):
+        self.length = length  
+        self.width = width  
+        self.height = height  
+        self.num_windows = num_windows  
+        self.window_area = window_area  
+        self.num_doors = num_doors  
+        self.door_area = door_area  
 
-    def площадь_рулона(self):
-        return self.ширина_рулона * self.длина_рулона
+    def wall_area(self):
+        perimeter = 2 * (self.length + self.width)
+        total_wall_area = perimeter * self.height
+        total_window_area = self.num_windows * self.window_area
+        total_door_area = self.num_doors * self.door_area
+        return total_wall_area - total_window_area - total_door_area
 
-def рассчитать_стоимость_обоев(комната, обои):
-    площадь_стен = комната.площадь_стен()
-    площадь_одного_рулона = обои.площадь_рулона()
+class Apartment:
+    def __init__(self, rooms):
+        self.rooms = rooms  
 
-    количество_рулонов = int(площадь_стен / площадь_одного_рулона)
-    if площадь_стен % площадь_одного_рулона != 0:
-        количество_рулонов += 1
+    def total_wall_area(self):
+        total_area = 0
+        for room in self.rooms:
+            total_area += room.wall_area()
+        return total_area
 
-    общая_стоимость = количество_рулонов * обои.цена_рулона
-    return количество_рулонов, общая_стоимость
+def calculate_wallpaper_cost(apartment, wallpaper_roll):
+    total_area = apartment.total_wall_area()
+    roll_area = wallpaper_roll.width * wallpaper_roll.length
+    num_rolls = int(total_area // roll_area)
+    if total_area % roll_area != 0:
+        num_rolls += 1
+
+    total_cost = num_rolls * wallpaper_roll.price_per_roll
+    return num_rolls, total_cost
+
+
+room1 = Room(length=5, width=4, height=3, num_windows=2, window_area=1.5, num_doors=1, door_area=2)
+room2 = Room(length=4, width=3, height=3, num_windows=1, window_area=1.5, num_doors=1, door_area=2)
+
+apartment = Apartment(rooms=[room1, room2])
+
+wallpaper_roll = WallpaperRoll(width=0.53, length=10, price_per_roll=20)
+num_rolls, total_cost = calculate_wallpaper_cost(apartment, wallpaper_roll)
+
+print(f"Number of rolls needed: {num_rolls}")
+print(f"Total cost: {total_cost}")
